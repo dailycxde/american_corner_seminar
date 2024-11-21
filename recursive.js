311dfc89aafb35dd01733f14f98b6968a30dd740
dailycxde <111638149+dailycxde@users.noreply.github.com>
2024-11-21 15:40:03 +0200

    Update recursive.js

--- recursive.js
+++ recursive.js
@@ -2,10 +2,5 @@ const fact = (n) => {
     if(n===1){
         return 1;
     }else{
-<<<<<<< HEAD
         return fact(n-1) * n + 1;
-=======
-        return fact(n-1) * n-1;
->>>>>>> amir
-    }
 }
